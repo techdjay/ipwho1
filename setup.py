d5dce112a4a3a67de8403996d160ba3f02c98c3f
techdjay <68823434+techdjay@users.noreply.github.com>
2023-07-08 16:33:09 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 setup(
-    name='kamabay-ipwhois',
+    name='ipwho',
     version='0.1.0',
     packages = find_packages(),
     include_package_data=True,
     description='ip information gathering.',
     long_description = README,
     author='Exso Kamabay',
-    url='https://github.com/ExsoKamabay/ipwhois',
+    url='https://github.com/techdjay/ipwho',
     license='Apache License 2.0',
     install_requires=['requests'],
     keywords = ['kamabay', 'ip', 'whois', 'information', 'gathering'],
