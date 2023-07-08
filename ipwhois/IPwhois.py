17a4df77810e0a4dcecd12eaaf6aea997c074eb0
techdjay <68823434+techdjay@users.noreply.github.com>
2023-07-08 15:24:21 +0530

    Update IPwhois.py
    Customized the code as per my requirement for daily uses.
    
    It will Perse and show only below important information's.
    
    i.e.
    # ipwho 1.1.1.1
    IP: 1.1.1.1
    Organization: APNIC and Cloudflare DNS Resolver project
    ISP: Cloudflare, Inc.

--- ipwhois/IPwhois.py
+++ ipwhois/IPwhois.py
@@ -1,20 +1,34 @@
+import sys
+import json
 from requests import get
 from json import dumps
 
 def ipwhois(ip4:str) -> str:
-    url = "https://ipwhois.app/json/%s"%(ip4);
+    url = "https://ipwhois.app/json/%s" % (ip4)
     try:
         req = get(url).json()
-        return json.dumps(req,indent=2,sort_keys=True)
-    except:return "Check your internet connection!"
+        parsed_data = {
+            "IP": req["ip"],
+            "Organization": req["org"],
+            "ISP": req["isp"]
+        }
+        formatted_output = "\n".join([f"{key}: {value}" for key, value in parsed_data.items()])
+        return formatted_output
+    except:
+        return "Check your internet connection!"
 
 def main():
-    while True:
-        print("type quit/exit for exit the program\n")
-        data = input("ip4 > ");
-        if not data:
-            print(ipwhois("0"));
-        elif data in ["quit","exit"]:
-            print("user quit :)");
-            False;break;
-        else:print(ipwhois(data));
+    if len(sys.argv) > 1:
+        ip_address = sys.argv[1]
+        print(ipwhois(ip_address))
+    else:
+        while True:
+            print("type quit/exit to exit the program\n")
+            data = input("ip4 > ")
+            if not data:
+                print(ipwhois("0"))
+            elif data in ["quit", "exit"]:
+                print("user quit :)")
+                break
+            else:
+                print(ipwhois(data))
