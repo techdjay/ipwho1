cd8db087763a69b99c7abe83dbee613455575576
techdjay <68823434+techdjay@users.noreply.github.com>
2023-07-08 16:24:51 +0530

    Update IPwhois.py

--- ipwho/IPwhois.py
+++ ipwho/IPwhois.py
@@ -3,7 +3,7 @@ import json
 from requests import get
 from json import dumps
 
-def ipwhois(ip4:str) -> str:
+def ipwho(ip4:str) -> str:
     url = "https://ipwhois.app/json/%s" % (ip4)
     try:
         req = get(url).json()
@@ -20,15 +20,15 @@ def ipwhois(ip4:str) -> str:
 def main():
     if len(sys.argv) > 1:
         ip_address = sys.argv[1]
-        print(ipwhois(ip_address))
+        print(ipwho(ip_address))
     else:
         while True:
             print("type quit/exit to exit the program\n")
             data = input("ip4 > ")
             if not data:
-                print(ipwhois("0"))
+                print(ipwho("0"))
             elif data in ["quit", "exit"]:
                 print("user quit :)")
                 break
             else:
-                print(ipwhois(data))
+                print(ipwho(data))
